95734119133160a6c26d5116493226e50639a25f
Choo Zhi Min <choozhimin@gmail.com>
2014-01-02 12:41:35 +0800

    change java version

--- makefile.bat
+++ makefile.bat
@@ -1,5 +1,5 @@
-"C:\Program Files\Java\jdk1.6.0_29\bin\javac" -cp bin -d bin .\src\sg\edu\nus\iss\jcalc\JCalcBuffer.java
-"C:\Program Files\Java\jdk1.6.0_29\bin\javac" -cp bin -d bin .\src\sg\edu\nus\iss\jcalc\JCalcList.java
-"C:\Program Files\Java\jdk1.6.0_29\bin\javac" -cp bin -d bin .\src\sg\edu\nus\iss\jcalc\JCalcMenuBar.java
-"C:\Program Files\Java\jdk1.6.0_29\bin\javac" -cp bin -d bin .\src\sg\edu\nus\iss\jcalc\JCalcStandardFrame.java
-"C:\Program Files\Java\jdk1.6.0_29\bin\javac" -cp bin -d bin .\src\sg\edu\nus\iss\jcalc\JCalc.java
+"C:\Program Files\Java\jdk1.6.0_45\bin\javac" -cp bin -d bin .\src\sg\edu\nus\iss\jcalc\JCalcBuffer.java
+"C:\Program Files\Java\jdk1.6.0_45\bin\javac" -cp bin -d bin .\src\sg\edu\nus\iss\jcalc\JCalcList.java
+"C:\Program Files\Java\jdk1.6.0_45\bin\javac" -cp bin -d bin .\src\sg\edu\nus\iss\jcalc\JCalcMenuBar.java
+"C:\Program Files\Java\jdk1.6.0_45\bin\javac" -cp bin -d bin .\src\sg\edu\nus\iss\jcalc\JCalcStandardFrame.java
+"C:\Program Files\Java\jdk1.6.0_45\bin\javac" -cp bin -d bin .\src\sg\edu\nus\iss\jcalc\JCalc.java
